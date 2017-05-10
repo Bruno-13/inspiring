73055c7bb37642246ec1665dd275309b44a4557d
Bruno-13 <bruno.rg.fernandes@gmail.com>
2017-05-10 19:52:49 -0400

    changed code

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 //lets stand up our first node.js server
 
 var http = require("http");
-var PORT = 7000;
+var PORT1 = 7000;
 var PORT2 = 7500;
 
-function handleRequest(request, response) { // (req, res)
-	response.end("Something inspiring blah blah blah " + request.url)
+function handleRequest1(request, response) { // (req, res)
+	response.end("Something inspiring blah blah blah ")
 }
 
 //set up our node http package
 
-var server = http.createServer(handleRequest);
+var server1 = http.createServer(handleRequest1);
 
-server.listen(PORT, function() {
-	console.log("Server listening on " + PORT);
+server1.listen(PORT1, function() {
+	console.log("Server listening on " + PORT1);
 });
 
 function handleRequest2(request, response) { // (req, res)
-	response.end("Something nice about my coding skills... it worked! " + request.url)
+	response.end("Something nice about my coding skills... this worked! ");
 }
 
 var server2 = http.createServer(handleRequest2);
-server.listen(PORT2, function() {
+server2.listen(PORT2, function() {
 	console.log("Server listening on " + PORT2);
 })
